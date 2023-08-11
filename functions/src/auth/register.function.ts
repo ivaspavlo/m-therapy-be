2ee6894968240538fa2113fafd4c7ae6a8b09196
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-08-11 22:28:49 +0200

    MT-7: Remove dotenv, use native env support

--- functions/src/auth/register.function.ts
+++ functions/src/auth/register.function.ts
@@ -1,15 +1,20 @@
 import { getFirestore } from 'firebase-admin/firestore';
 import { onRequest } from 'firebase-functions/v2/https';
 import { Request, Response } from 'firebase-functions';
+import { defineString } from 'firebase-functions/params';
+
+const welcomeMessage = defineString('WELCOME_MESSAGE');
 
 
 export const RegisterFunction = onRequest(
   // { cors: CORS_URLS, enforceAppCheck: true },
   async (req: Request, res: Response) => {
+    console.log(welcomeMessage);
     const original = req.query.text;
     const writeResult = await getFirestore()
       .collection('messages')
       .add({original: original});
+
     res.json({result: `Message with ID: ${writeResult.id} added.`});
   }
 );
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,83 +1,10 @@
-import { Response, Request, logger } from 'firebase-functions';
-import { onRequest } from 'firebase-functions/v2/https';
-import { onDocumentCreated } from 'firebase-functions/v2/firestore';
+// import { logger } from 'firebase-functions';
+// import { onRequest } from 'firebase-functions/v2/https';
+// import { onDocumentCreated } from 'firebase-functions/v2/firestore';
+// import { getFirestore } from 'firebase-admin/firestore';
 import { initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
 import { RegisterFunction } from './auth';
 
 initializeApp();
 
 exports.register = RegisterFunction;
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req: any, res: any) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore()
-      .collection("messages")
-      .add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
-
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event: any) => {
-  // Grab the current value of what was written to Firestore.
-  const original = event.data.data().original;
-
-  // Access the parameter `{documentId}` with `event.params`
-  logger.log("Uppercasing", event.params.documentId, original);
-
-  const uppercase = original.toUpperCase();
-
-  // You must return a Promise when performing
-  // asynchronous tasks inside a function
-  // such as writing to Firestore.
-  // Setting an 'uppercase' field in Firestore document returns a Promise.
-  return event.data.ref.set({uppercase}, {merge: true});
-});
-
-exports.registerUser = onRequest(
-  { enforceAppCheck: true }, async (req: Request, res: Response) => {
-    let user;
-
-    try {
-      user = JSON.parse(req.body);
-    } catch (e: any) {
-      logger.log(e);
-    }
-
-    console.log(user);
-
-    res.json({result: `User with ID: ${1} created.`});
-  }
-);
-
-
-
-
-// _______________________________________________________ //
-
-// Debugger instructions https://medium.com/firebase-developers/debugging-firebase-functions-in-vs-code-a1caf22db0b2
-// update package.json with this "build": "npm run lint && tsc"
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info('Hello logs!', {structuredData: true});
-//   response.send('Hello from Firebase!');
-// });
-
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from 'firebase-functions/v2/https';
- * import {onDocumentWritten} from 'firebase-functions/v2/firestore';
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
