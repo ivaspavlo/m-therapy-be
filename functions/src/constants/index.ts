4f99d0ab4384701d3b855198764fdcfc93acbaf3
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-08-19 15:53:26 +0200

    MT-12: Implement validation for register request

--- functions/src/constants/index.ts
+++ functions/src/constants/index.ts
@@ -2,7 +2,8 @@ export const CORS_URLS: string[] = [];
 
 export enum ERROR_MESSAGES {
   GENERAL = 'Internal server error',
-  DUPLICATE = 'Instance already exists'
+  DUPLICATE = 'Instance already exists',
+  FIELDS_VALIDATION = 'Validation for fields failed'
 };
 
 export enum COLLECTIONS {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,3 @@
-// import { logger } from 'firebase-functions';
-// import { onRequest } from 'firebase-functions/v2/https';
-// import { onDocumentCreated } from 'firebase-functions/v2/firestore';
-// import { getFirestore } from 'firebase-admin/firestore';
 import { initializeApp } from 'firebase-admin/app';
 import { RegisterFunction } from './auth';
 
