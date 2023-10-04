4ef81e5194abe34e23a9c289ffa7e779812abdcd
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-10-04 08:56:30 +0200

    MT-32.5: Partially implement tests for register function

--- functions/test/test.online.ts
+++ functions/test/test.online.ts
@@ -19,28 +19,26 @@ describe('MT online test suite', () => {
     testBase.cleanup();
   });
 
-  describe('register function', () => {
-    it('should return true', () => {
+  describe('register', () => {
 
-      // Create a DataSnapshot with the value 'input' and the reference path 'messages/11111/original'.
-      const snap = testBase.database.makeDataSnapshot('input', 'messages/11111/original');
-      console.log(snap);
-      console.log(myFunctions);
-      assert.equal(true, true);
+    it('should create a user in DB', () => {
+      const req = {
+        body: {
+          firstname: 'Test',
+          lastname: 'Testovich',
+          email: 'testovichus@gmail.com',
+          birthday: 589050238388,
+          phone: '+111222333444',
+          password: 'TestPass1!'
+        }
+      };
+      const res = {
 
-      // Wrap the makeUppercase function
-      // const wrapped = test.wrap(myFunctions.makeUppercase);
-      // Call the wrapped function with the snapshot you constructed.
-      // return wrapped(snap).then(() => {
-        // Read the value of the data at messages/11111/uppercase. Because `admin.initializeApp()` is
-        // called in functions/index.js, there's already a Firebase app initialized. Otherwise, add
-        // `admin.initializeApp()` before this line.
-        // return database().ref('messages/11111/uppercase').once('value').then((createdSnap) => {
-          // Assert that the value is the uppercased version of our input.
-      //     assert.equal(createdSnap.val(), 'INPUT');
-      //   });
-      // });
-    })
+      };
+
+      myFunctions.register(req, res);
+    });
+  
   });
 
   // describe('addMessage', () => {
@@ -67,3 +65,4 @@ describe('MT online test suite', () => {
 })
 
 // https://medium.com/@leejh3224/testing-firebase-cloud-functions-with-jest-4156e65c7d29
+// https://firebase.google.com/docs/reference/functions/test/test.database
