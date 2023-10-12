8a10f4b759548708cc0a21bbd817998f70f57ce8
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-10-12 12:45:34 +0200

    MT-32.13: Adjust files path for debugging

--- functions/tests/test.online.ts
+++ functions/tests/test.online.ts
@@ -1,14 +1,13 @@
 // import { database } from 'firebase-admin';
-import * as path from 'path';
 
 
 const testBase = require('firebase-functions-test')({
   projectId: process.env.GCLOUD_PROJECT,
   databaseURL: 'https://mt-stage-db6be.firebaseio.com'
-}, path.resolve('mt-stage-db6be-a531eb8c5a6b.json'));
+}, `${__dirname}/../mt-stage-db6be-a531eb8c5a6b.json`);
 
 // Should be after firebase-functions-test is initialized.
-const myFunctions = require(path.resolve('./src/index.ts'));
+const myFunctions = require(`${__dirname}/../src/index.ts`);
 
 describe('MT cloud functions', () => {
 
@@ -37,8 +36,11 @@ describe('MT cloud functions', () => {
         })
       };
       await myFunctions.register(req, res);
-      // bEvmR3XShXJglLVYBA7I
-      // bEvmR3XShXJglLVYBA7I
+
+      
+
+      expect(1).toBe(1);
+
     });
   
   });
@@ -50,3 +52,4 @@ describe('MT cloud functions', () => {
 // https://basarat.gitbook.io/typescript/intro-1/jest
 // https://stackoverflow.com/questions/70442193/error-wrap-function-is-only-available-for-oncall-http-functions-not-onreque
 // https://medium.com/@leejh3224/testing-firebase-cloud-functions-with-jest-4156e65c7d29
+// https://medium.com/practical-coding/set-up-gitlab-ci-cd-for-testing-your-firebase-functions-49878b3e7764
