31157d8a6241d07526403b71a0985e6d1addf695
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-11-08 09:33:32 +0100

    MT-36.1: Fix ts configuration issue

--- functions/src/manager/manager.function.ts
+++ functions/src/manager/manager.function.ts
@@ -1,13 +1,22 @@
 import { onRequest } from 'firebase-functions/v2/https';
 import { Request, Response } from 'firebase-functions';
-import { ENV_KEYS } from 'src/shared/constants';
-import { ResponseBody } from 'src/shared/models';
-
+import { ENV_KEYS, ERROR_MESSAGES } from '../shared/constants';
+import { ResponseBody } from '../shared/models';
+import * as logger from 'firebase-functions/logger';
+import * as jwt from 'jsonwebtoken';
 
 export const ManagerFunction = onRequest(
   { secrets: [ENV_KEYS.JWT_SECRET] },
   async (req: Request, res: Response): Promise<void> => {
-    const jwt = req.headers.authorization;
-    res.status(400).send(new ResponseBody({jwt}, true));
+    const clientJWT = req.headers.authorization;
+    try {
+      jwt.verify(clientJWT as string, process.env[ENV_KEYS.JWT_SECRET] as string);
+    } catch (e: any) {
+      res.status(401).json(new ResponseBody(null, false, [ERROR_MESSAGES.JWT]));
+      return;
+    }
+
+    logger.info("Retrieved resource");
+    res.status(200).send(new ResponseBody({jwt}, true));
   }
 );
