7c8e1caea738bb70fefa516bf4cfac80ab4927fd
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2023-12-09 21:33:51 +0100

    MT-45: Complete user service

--- functions/src/manager/user.function.ts
+++ functions/src/manager/user.function.ts
@@ -1,22 +1,63 @@
 import { onRequest } from 'firebase-functions/v2/https';
 import { Request, Response } from 'firebase-functions';
-import { ENV_KEYS, ERROR_MESSAGES } from '../shared/constants';
+import { COLLECTIONS, ENV_KEYS, ERROR_MESSAGES } from '../shared/constants';
 import { ResponseBody } from '../shared/models';
 import * as logger from 'firebase-functions/logger';
 import * as jwt from 'jsonwebtoken';
+import { QueryDocumentSnapshot, QuerySnapshot, getFirestore } from 'firebase-admin/firestore';
+import { IUser } from 'src/shared/interfaces';
+
 
 export const UserFunction = onRequest(
   { secrets: [ENV_KEYS.JWT_SECRET] },
   async (req: Request, res: Response): Promise<void> => {
+    const generalError = new ResponseBody(null, false, [ERROR_MESSAGES.GENERAL]);
+    const jwtError = new ResponseBody(null, false, [ERROR_MESSAGES.JWT]);
+
     const clientJWT = req.headers.authorization;
+
+    if (!clientJWT) {
+      res.status(401).json(jwtError);
+    }
+
     try {
       jwt.verify(clientJWT as string, process.env[ENV_KEYS.JWT_SECRET] as string);
     } catch (e: any) {
-      res.status(401).json(new ResponseBody(null, false, [ERROR_MESSAGES.JWT]));
+      res.status(401).json(jwtError);
       return;
     }
 
-    logger.info("Retrieved resource");
-    res.status(200).send(new ResponseBody({jwt}, true));
+    let parsedResetToken: { [key:string]: string, email: string };
+    try {
+      parsedResetToken = JSON.parse(Buffer.from(clientJWT!.split('.')[1], 'base64').toString());
+    } catch (e: any) {
+      res.status(401).json(jwtError);
+      return;
+    }
+
+    let queryByEmail: QuerySnapshot;
+    try {
+      queryByEmail = await getFirestore().collection(COLLECTIONS.USERS).where('email', '==', parsedResetToken.email).get();
+    } catch(e: any) {
+      logger.error('[Reset] Querying DB by email failed', e);
+      res.status(500).json(generalError);
+      return;
+    }
+
+    if (queryByEmail.empty) {
+      res.status(400).json(new ResponseBody(null, false, [ERROR_MESSAGES.NOT_FOUND]));
+      return;
+    }
+
+    const userDocumentSnapshot: QueryDocumentSnapshot | undefined = queryByEmail.docs.find(d => !!d);
+    if (!userDocumentSnapshot || !userDocumentSnapshot.data()) {
+      res.status(401).json(new ResponseBody(null, false, [ERROR_MESSAGES.CREDENTIALS]));
+      return;
+    }
+
+    const user: IUser = userDocumentSnapshot.data() as IUser;
+
+    logger.info(`Retrieved user data: ${user.id}`);
+    res.status(200).send(new ResponseBody({user}, true));
   }
 );
