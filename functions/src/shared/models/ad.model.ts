60f3bd61048ed1c66b3cc04ab363ec3d24926cda
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-01-22 10:21:16 +0100

    MT-58.3: Complete ad function, partially implement unit test

--- functions/src/shared/models/ad.model.ts
+++ functions/src/shared/models/ad.model.ts
@@ -1,3 +1,25 @@
-export class Ad {
+import { AD_TYPE } from '../constants';
+import { IAd, IAdDB } from '../interfaces';
+
+export class Ad implements IAd {
+
+  public type!: AD_TYPE;
+  public title!: string;
+  public content!: string;
+  public endDate?: Date;
+  public link?: string;
+
   constructor() {}
+
+  public static of(
+    value: IAdDB
+  ): Ad {
+    const ad = new Ad();
+    ad.type = value.type;
+    ad.title = value.title;
+    ad.content = value.content;
+    ad.endDate = value.endDate ? new Date(value.endDate) : undefined;
+    ad.link = value.link;
+    return ad;
+  }
 }
