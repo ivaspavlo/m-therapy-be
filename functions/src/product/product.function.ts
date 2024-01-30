364e5138189881bae64b1b55142691141dd5d23f
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-01-30 11:09:21 +0100

    MT-60: Partially implement get products function

--- functions/src/product/product.function.ts
+++ functions/src/product/product.function.ts
@@ -1,11 +1,27 @@
 import { onRequest } from 'firebase-functions/v2/https';
-import { Request, Response } from 'firebase-functions';
-import { ENV_KEYS } from '../shared/constants';
+import { Request, Response, logger } from 'firebase-functions';
+import { getFirestore } from 'firebase-admin/firestore';
+import { IProduct } from 'src/shared/interfaces/product.interface';
+import { COLLECTIONS, ERROR_MESSAGES } from 'src/shared/constants';
+import { ResponseBody } from 'src/shared/models';
 
 
 export const ProductFunction = onRequest(
-  { secrets: [ENV_KEYS.JWT_SECRET] },
   async (req: Request, res: Response): Promise<void> => {
-    
+    switch(req.method) {
+    case('GET'): return getProducts(res);
+    // update and delete methods to be implemented
+    }
   }
 );
+
+async function getProducts(res: Response): Promise<void> {
+  try {
+    const products: IProduct[] = (await getFirestore().collection(COLLECTIONS.PRODUCTS).get()).docs.map(d => d.data() as IProduct);
+    logger.info('[GET PRODUCTS] Retrieval data for products successful');
+    res.status(200).json(new ResponseBody(products, true));
+  } catch (e: any) {
+    logger.error('[GET AD] Retrieval data for ads failed: ', e);
+    res.status(500).json(new ResponseBody(null, false, [ERROR_MESSAGES.GENERAL]));
+  }
+}
