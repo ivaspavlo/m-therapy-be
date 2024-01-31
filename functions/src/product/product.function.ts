80ca5ceb98a8ba8bd72048e11b56a854118d9348
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-01-31 20:59:39 +0100

    MT-60.1: Implement unit tests for product function

--- functions/src/product/product.function.ts
+++ functions/src/product/product.function.ts
@@ -1,10 +1,9 @@
 import { onRequest } from 'firebase-functions/v2/https';
 import { Request, Response, logger } from 'firebase-functions';
 import { getFirestore } from 'firebase-admin/firestore';
-import { IProduct } from 'src/shared/interfaces/product.interface';
-import { COLLECTIONS, ERROR_MESSAGES } from 'src/shared/constants';
-import { ResponseBody } from 'src/shared/models';
-
+import { IProduct } from '../shared/interfaces/product.interface';
+import { COLLECTIONS, ERROR_MESSAGES } from '../shared/constants';
+import { ResponseBody } from '../shared/models';
 
 export const ProductFunction = onRequest(
   async (req: Request, res: Response): Promise<void> => {
@@ -21,7 +20,7 @@ async function getProducts(res: Response): Promise<void> {
     logger.info('[GET PRODUCTS] Retrieval data for products successful');
     res.status(200).json(new ResponseBody(products, true));
   } catch (e: any) {
-    logger.error('[GET AD] Retrieval data for ads failed: ', e);
+    logger.error('[GET PRODUCTS] Retrieval data for products failed: ', e);
     res.status(500).json(new ResponseBody(null, false, [ERROR_MESSAGES.GENERAL]));
   }
 }
