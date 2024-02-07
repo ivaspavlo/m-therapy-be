250345a44ca9d25589bcccb8a651910bdc55c564
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-02-07 20:55:49 +0100

    MT-61.2: Add contacts to CMS function

--- functions/src/content/content.function.ts
+++ functions/src/content/content.function.ts
@@ -5,6 +5,7 @@ import { IProduct } from '../shared/interfaces/product.interface';
 import { COLLECTIONS, ERROR_MESSAGES } from '../shared/constants';
 import { Ad, ResponseBody } from '../shared/models';
 import { IAd } from '../shared/interfaces';
+import { IContact } from 'src/shared/interfaces/contact.interface';
 
 export const ContentFunction = onRequest(
   async (req: Request, res: Response): Promise<void> => {
@@ -19,8 +20,9 @@ async function getContent(res: Response): Promise<void> {
   try {
     const products: IProduct[] = (await getFirestore().collection(COLLECTIONS.PRODUCTS).get()).docs.map(d => d.data() as IProduct);
     const ads: IAd[] = (await getFirestore().collection(COLLECTIONS.ADS).get()).docs.map(d => Ad.of(d.data() as IAd)) as IAd[];
+    const contacts: IContact[] = (await getFirestore().collection(COLLECTIONS.CONTACTS).get()).docs.map(d => d.data() as IContact);
     logger.info('[GET CONTENT] Retrieved successfully');
-    res.status(200).json(new ResponseBody({products, ads}, true));
+    res.status(200).json(new ResponseBody({products, ads, contacts}, true));
   } catch (e: any) {
     logger.error('[GET CONTENT] Retrieval failed: ', e);
     res.status(500).json(new ResponseBody(null, false, [ERROR_MESSAGES.GENERAL]));
